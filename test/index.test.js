f2df16f5d5444d0319ada84b33f9f9cbaf04fea9
Nadjib Amar <hello@ebabel.eu>
2017-11-21 02:28:56 +0100

    documented the unit test to check if the dice is fair

--- test/index.test.js
+++ test/index.test.js
@@ -27,7 +27,7 @@ describe('randomNumber', () => {
     Number.isInteger(randomNumber(10, 3).toInt()).should.be.true;
   });
 
-  it('evenly spreads 100000 random results for a six sided dice', () => {
+  it('reasonably spreads 100000 random results for a six sided dice', () => {
     // Total number of times the six sided dice is thrown.
     const totalThrown = 100000;
 
@@ -37,27 +37,33 @@ describe('randomNumber', () => {
     // Acceptable percentage deviation from optimum.
     const acceptableDeviation = 2;  // i.e. 2% here.
 
-    // Results of throwing the six sided dice many times.
-    const results = new Array(totalThrown).fill(0)
-      .map(value => randomNumber(7, 1).toInt())
-      .sort()
-      .reduce((acc, val) => acc.set(val, 1 + (acc.get(val) || 0)), new Map());
-
     // Flag default value: the spread is reasonable, i.e. percentageDeviation isn't too great.
     let isSpreadReasonable = true;
-        
-    // Calculate absolute and average deviations from results to spread.
-    // value: count of how many times a dice face value has been found.
-    // key: face value of a dice.
-    results.forEach((value, key) => {
-      const absoluteDeviation = Math.abs(value - (optimum * totalThrown));
-      const percentageDeviation = (absoluteDeviation / (optimum * totalThrown)) * 100
 
-      if (percentageDeviation > acceptableDeviation) {
-        console.error(`Dice face value ${key} percentage deviation from optimum is ${percentageDeviation}%.`);
-        isSpreadReasonable = false;
-      }
-    });
+    // Throw the six sided dice many times.
+    new Array(totalThrown).fill(0)
+      .map(value => randomNumber(7, 1).toInt())
+      // Sorting the raw results of all dice throws makes the reduce work at least 20% faster.
+      // i.e. 77 to 80ms instead of 100 to 104 ms.
+      .sort()
+      // Create a Map with key and value where key is the face value of
+      // the dice and the value is how many times that face value has been thrown.
+      // See https://stackoverflow.com/a/46090384/97292
+      // Note: the Map isn't the "map" function from arrays.
+      // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
+      .reduce((acc, val) => acc.set(val, 1 + (acc.get(val) || 0)), new Map())
+      // Calculate absolute and average deviations for each dice face value (6 times, not 100000 times!).
+      // value: count of how many times a dice face value has been found.
+      // key: face value of a dice.
+      .forEach((value, key) => {
+        const absoluteDeviation = Math.abs(value - (optimum * totalThrown));
+        const percentageDeviation = (absoluteDeviation / (optimum * totalThrown)) * 100;
+
+        if (percentageDeviation > acceptableDeviation) {
+          console.error(`Dice face value ${key} percentage deviation from optimum is ${percentageDeviation}%.`);
+          isSpreadReasonable = false;
+        }
+      });
 
     isSpreadReasonable.should.be.true;
   });
