c2745e5dc16d3458f266add5637739cabe749533
Nadjib Amar <hello@ebabel.eu>
2017-11-21 02:00:57 +0100

    test spread of random values

--- test/index.test.js
+++ test/index.test.js
@@ -26,4 +26,39 @@ describe('randomNumber', () => {
   it('toInt returns an integer', () => {
     Number.isInteger(randomNumber(10, 3).toInt()).should.be.true;
   });
+
+  it('evenly spreads 100000 random results for a six sided dice', () => {
+    // Total number of times the six sided dice is thrown.
+    const totalThrown = 100000;
+
+    // Optimum frequency of each dice face value being found in the results.
+    const optimum = 1/6;
+
+    // Acceptable percentage deviation from optimum.
+    const acceptableDeviation = 2;  // i.e. 2% here.
+
+    // Results of throwing the six sided dice many times.
+    const results = new Array(totalThrown).fill(0)
+      .map(value => randomNumber(7, 1).toInt())
+      .sort()
+      .reduce((acc, val) => acc.set(val, 1 + (acc.get(val) || 0)), new Map());
+
+    // Flag default value: the spread is reasonable, i.e. percentageDeviation isn't too great.
+    let isSpreadReasonable = true;
+        
+    // Calculate absolute and average deviations from results to spread.
+    // value: count of how many times a dice face value has been found.
+    // key: face value of a dice.
+    results.forEach((value, key) => {
+      const absoluteDeviation = Math.abs(value - (optimum * totalThrown));
+      const percentageDeviation = (absoluteDeviation / (optimum * totalThrown)) * 100
+
+      if (percentageDeviation > acceptableDeviation) {
+        console.error(`Dice face value ${key} percentage deviation from optimum is ${percentageDeviation}%.`);
+        isSpreadReasonable = false;
+      }
+    });
+
+    isSpreadReasonable.should.be.true;
+  });
 });
