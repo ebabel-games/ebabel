fb86c15e754594e12cc8ff4df102fc208dc0e2e1
Nadjib Amar <hello@ebabel.eu>
2017-11-27 10:54:46 +0100

    version and more unit tests

--- test/count-array-elements.test.js
+++ test/count-array-elements.test.js
@@ -1,5 +1,5 @@
 // Import chai as our assertion library.
-import chai from 'chai';
+import chai, { expect } from 'chai';
 
 // Require jsdom-global and run.
 require('jsdom-global')()
@@ -26,9 +26,44 @@ describe('countArrayElements', () => {
     result.pear.should.be.equal(3);
   });
 
+  it('counts the occurences of a mixure elements who have different primitive types', () => {
+    const result = countArrayElements([
+      'red car',
+      Math.PI,
+      'red car',
+      3.14159,
+      Math.PI,
+      42,
+      Math.PI,
+      Math.PI,
+      'red car',
+      42,
+    ]);
+
+    result['red car'].should.be.equal(3);
+    result[Math.PI].should.be.equal(4);
+    result[42].should.be.equal(2);
+    result[3.14159].should.be.equal(1);
+  });
+
+  it('does not count occurences of elements that were not present in the input array', () => {
+    const result = countArrayElements([
+      'blanket',
+      'michael jackson',
+    ]);
+
+    expect(result['blanket jackson']).to.equal(undefined);
+  });
+
   it('throws an error when input array is missing', () => {
     (() => {
       countArrayElements();
+    }).should.throw('Missing array, so it is not possible to count members of that array.');
+  });
+
+  it('throws an error when input is a string', () => {
+    (() => {
+      countArrayElements('inputString');
     }).should.throw('Missing array, so it is not possible to count members of that array.');
   });
 });
