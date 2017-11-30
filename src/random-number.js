d546973bca828bc8e36c0e6bca9853dcb8b85a08
Nadjib Amar <hello@ebabel.eu>
2017-11-30 17:35:41 +0100

    finished fixing and documenting randomNumber

--- src/random-number.js
+++ src/random-number.js
@@ -2,9 +2,9 @@
 
 /**
  * `randomNumber`
- * Returns a random integer number from 0 to max or from min to max (min is non-inclusive).
- * max: Number
- * min?: Number
+ * Returns a random integer number from 1 to max or from min to max (min is non-inclusive).
+ * @param {number} max - The maximum value that can be randomly returned. This value is included in the possible values.
+ * @param {number?} min - The optional minimum value that can be randomly returned. This value is not included in the possible values, therefore to include 0, set min to -1. When min is not set, the minimum value will be 0, so 1 can be returned, which is the most common case in games.
  */
 const randomNumber = (max, min = 0) => {
   if (min > max) {
