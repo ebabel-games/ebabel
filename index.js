cc4644c4b947aa7e656eeea887a7a825461679e0
Thomas Amar <hello@ebabel.eu>
2018-09-29 00:47:52 +0200

    fix the name import for a module

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const random = require('ebabel-random');
 const guid = require('ebabel-guid');
 const randomList = require('ebabel-random-list');
 const Ido = require('ebabel-ido');
-const preventXss = require('.ebabel-prevent-xss');
+const preventXss = require('ebabel-prevent-xss');
 const countArrayElements = require('ebabel-count-array-elements');
 const distance = require('ebabel-distance');
 
