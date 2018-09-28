4f7b3b250c7c2a261da8e6743474ccf8bae7e430
Thomas Amar <hello@ebabel.eu>
2018-09-28 23:40:43 +0200

    remove guid from library and recreate it as a separate module

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const random = require('ebabel-random');
+const guid = require('ebabel-guid');
 
 const randomList = require('./modules/random-list.js');
-const guid = require('./modules/guid.js');
 const countArrayElements = require('./modules/count-array-elements.js');
 const Ido = require('./modules/ido.js');
 const preventXss = require('./modules/prevent-xss.js');
