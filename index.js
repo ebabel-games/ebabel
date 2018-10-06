2ec450c0ad97f4ee2cd43886ed668dbf8753b3cb
Thomas Amar <hello@ebabel.eu>
2018-10-06 16:54:47 +0200

    Remove modules that have been refactored into ebabel-utils.

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 'use strict';
 
-const random = require('ebabel-random');
-const guid = require('ebabel-guid');
-const randomList = require('ebabel-random-list');
-const Ido = require('ebabel-ido');
-const preventXss = require('ebabel-prevent-xss');
-const countArrayElements = require('ebabel-count-array-elements');
-const distance = require('ebabel-distance');
+const { hexStringToInt, mockTHREEx, mockTHREE, mockDataStore } = require('ebabel-mocks');
+
+const {
+  deepCopy,
+  dice,
+  positive,
+  randomPosition,
+  randomPosOrNeg,
+  reducedDistance,
+  trait,
+  countArrayElements,
+  distance,
+  guid,
+  Ido,
+  preventXss,
+  random,
+  randomList,
+} = require('ebabel-utils');
+
+const { keyboardControls, updatePlayerPositionRotation } = require('ebabel-keyboard-controls');
 const light = require('ebabel-light');
 const boundingBox = require('ebabel-bounding-box');
 const skybox = require('ebabel-skybox');
-const { hexStringToInt, mockTHREEx, mockTHREE, mockDataStore } = require('ebabel-mocks');
-const { deepCopy, dice, positive, randomPosition, randomPosOrNeg, reducedDistance, trait } = require('ebabel-utils');
-const { keyboardControls, updatePlayerPositionRotation } = require('ebabel-keyboard-controls');
 
 module.exports = {
   random,
